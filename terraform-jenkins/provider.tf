6cdb71735cbfcc5f7f59f7231b979ff7e3a8e805
manasjia <manasjia@gmail.com>
2024-07-25 21:02:11 +0530

    credentials added

--- terraform-jenkins/provider.tf
+++ terraform-jenkins/provider.tf
@@ -10,5 +10,7 @@ terraform {
 
 provider "aws" {
     region = "us-east-1"
+    access_key = "yoAKIASVYSAJ7N4VKRKB4Y" # Optional, can also use environment variables
+    secret_key = "Kr1xjImcNxA00vJtGX9s5PYtcLpR0LCz1141OFos" # Optional, can also use environment variables
   
 }
