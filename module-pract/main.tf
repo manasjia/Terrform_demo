34e4ba5ce2baef4aaf6c40ce113412386d41b20c
manasjia <manasjia@gmail.com>
2024-07-25 21:18:58 +0530

    credentials added

--- module-pract/main.tf
+++ module-pract/main.tf
@@ -1,13 +1,13 @@
 module "vpc" {
-    
-    source = "./modules/vpc"
-    region = var.region
-    vpc_cidr = var.vpc_cidr
-    public_subnet_az1_cidr = var.public_subnet_az1_cidr
-    public_subnet_az2_cidr = var.public_subnet_az2_cidr
-    private_subnet_az1_cidr = var.private_subnet_az1_cidr
-    private_subnet_az2_cidr = var.private_subnet_az2_cidr
-    projectname = var.projectname
-  
-  
+
+  source                  = "./modules/vpc"
+  region                  = var.region
+  vpc_cidr                = var.vpc_cidr
+  public_subnet_az1_cidr  = var.public_subnet_az1_cidr
+  public_subnet_az2_cidr  = var.public_subnet_az2_cidr
+  private_subnet_az1_cidr = var.private_subnet_az1_cidr
+  private_subnet_az2_cidr = var.private_subnet_az2_cidr
+  projectname             = var.projectname
+
+
 }
--- module-pract/provider.tf
+++ module-pract/provider.tf
@@ -1,16 +1,16 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.33.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.33.0"
 
     }
-    
+
   }
 }
 
 provider "aws" {
-    region = var.region
-    
-  
+  region = var.region
+
+
 }
